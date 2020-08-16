05bfa4ee95a77fa70a90f6d92ac1296828182279
gagandeep.singh <gagandeep.singh@ebizontek.com>
2020-08-16 23:04:53 +0530

    Updates

---
+++ precache-manifest.0a1498ccfbeab6ad1f3a7b58488ef2a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389f981eeadc37fddb14e09369801fc3",
+    "url": "/Calci/index.html"
+  },
+  {
+    "revision": "cf44774fdbbdd204d7fb",
+    "url": "/Calci/static/css/main.a6aff8d5.chunk.css"
+  },
+  {
+    "revision": "1e8019f618bdaee92e35",
+    "url": "/Calci/static/js/2.4796995a.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/Calci/static/js/2.4796995a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf44774fdbbdd204d7fb",
+    "url": "/Calci/static/js/main.8805a0d7.chunk.js"
+  },
+  {
+    "revision": "6e8502ea9e89948728fc",
+    "url": "/Calci/static/js/runtime-main.5f19205f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calci/static/media/logo.5d5d9eef.svg"
+  }
+]);
